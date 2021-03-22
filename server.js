77e15613a74b1c1ea9623dfd08ef6780376e6e17
Martin M <martin7mondaca@gmail.com>
2021-03-21 22:07:04 -0700

    added morgan npm package requirement

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 // Requiring necessary npm packages
-var express = require("express");
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 // Setting up port 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
-var app = express();
+const app = express();
+
+app.use(logger("dev"))
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // require("./routes/html-routes.js")(app);
 // require("./routes/api-routes.js")(app);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbWorkout", { useNewUrlParser: true });
 
 app.listen(PORT, function () {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
