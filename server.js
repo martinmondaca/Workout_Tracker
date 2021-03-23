9009bebbbfb8655c58202e47a40503530e8ab53d
Martin M <martin7mondaca@gmail.com>
2021-03-23 00:05:21 -0700

    added useFindAndModify: false to mongoose connectoin

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workoutdb",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
 //routes
 app.use(require("./routes/api-routes.js"))
