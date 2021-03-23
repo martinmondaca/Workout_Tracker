11fd3504b9be399e342b1beafbf621770b2184b9
Martin M <martin7mondaca@gmail.com>
2021-03-22 23:45:44 -0700

    updated day in seeds for correct day formatting

--- seeders/seed.js
+++ seeders/seed.js
@@ -8,7 +8,7 @@ mongoose.connect("mongodb://localhost/workoutdb", {
 
 let workoutSeed = [
   {
-    day: new Date().setDate(new Date().getDate() - 10),
+    day: new Date(new Date().setDate(new Date().getDate() - 10)),
     exercises: [
       {
         type: "resistance",
@@ -21,7 +21,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate() - 9),
+    day: new Date(new Date().setDate(new Date().getDate() - 9)),
     exercises: [
       {
         type: "resistance",
@@ -34,7 +34,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate() - 8),
+    day: new Date(new Date().setDate(new Date().getDate() - 8)),
     exercises: [
       {
         type: "resistance",
@@ -47,7 +47,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate() - 7),
+    day: new Date(new Date().setDate(new Date().getDate() - 7)),
     exercises: [
       {
         type: "cardio",
@@ -58,7 +58,7 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate() - 6),
+    day: new Date(new Date().setDate(new Date().getDate() - 6)),
     exercises: [
       {
         type: "resistance",
@@ -71,15 +71,13 @@ let workoutSeed = [
     ]
   },
   {
-    day: new Date().setDate(new Date().getDate() - 5),
+    day: new Date(new Date().setDate(new Date().getDate() - 5)),
     exercises: [
       {
-        type: "resistance",
-        name: "Bench Press",
-        duration: 20,
-        weight: 300,
-        reps: 10,
-        sets: 4
+        type: "cardio",
+        name: "Running",
+        duration: 25,
+        distance: 4
       }
     ]
   },
